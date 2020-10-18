5631a712e384ae691276afcd95c0345aa52feb06
Thymen Rijpkema <t.d.j.rijpkema@gmail.com>
2020-10-18 12:12:08 +0200

    Corrected input checker

--- sudoku/pycosatpy/main.py
+++ sudoku/pycosatpy/main.py
@@ -9,47 +9,45 @@ from sudoku.pycosatpy.solver import SudokuSolverPycosat
 def input_check(flatline, sudoku, iterable=SIMPLE_SUDOKU + HARD_SUDOKU):
     """ Check the inputs on data, otherwise pick a random sudoku from the iterable.  """
     if flatline is None and sudoku is None:
-        sudoku = Sudoku(random.choice(iterable))
-    elif sudoku is None:
-        sudoku = Sudoku(flatline)
-    return sudoku
+        flatline = random.choice(iterable)
+    return flatline, sudoku
 
 
 def solve_normal(flatline=None, sudoku=None):
     # If nothing is provided, we automatically get a normal sudoku.
-    sudoku = input_check(flatline, sudoku)
-    solver = SudokuSolverPycosat(sudoku)
+    flatline, sudoku = input_check(flatline, sudoku)
+    solver = SudokuSolverPycosat(flatline, sudoku)
     solver.run_all()
     solver.show()
 
 
 def solve_knight_move_constraint(flatline=None, sudoku=None):
-    sudoku = input_check(flatline, sudoku, KNIGHT_CONSTRAINT)
-    solver = SudokuSolverPycosat(sudoku)
+    flatline, sudoku = input_check(flatline, sudoku, KNIGHT_CONSTRAINT)
+    solver = SudokuSolverPycosat(flatline, sudoku)
     solver.add_knight_move_constraint()
     solver.run()
     solver.show()
 
 
 def solve_kings_move_constraint(flatline=None, sudoku=None):
-    sudoku = input_check(flatline, sudoku, KINGS_MOVE_CONSTRAINT)
-    solver = SudokuSolverPycosat(sudoku)
+    flatline, sudoku = input_check(flatline, sudoku, KINGS_MOVE_CONSTRAINT)
+    solver = SudokuSolverPycosat(flatline, sudoku)
     solver.add_kings_move_constraint()
     solver.run()
     solver.show()
 
 
 def solve_non_consecutive_constraint(flatline=None, sudoku=None):
-    sudoku = input_check(flatline, sudoku, NON_CONSECUTIVE_CONSTRAINT)
-    solver = SudokuSolverPycosat(sudoku)
+    flatline, sudoku = input_check(flatline, sudoku, NON_CONSECUTIVE_CONSTRAINT)
+    solver = SudokuSolverPycosat(flatline, sudoku)
     solver.add_non_consecutive_constraint()
     solver.run()
     solver.show()
 
 
 def solve_miracle(flatline=None, sudoku=None):
-    sudoku = input_check(flatline, sudoku, MIRACLE)
-    solver = SudokuSolverPycosat(sudoku)
+    flatline, sudoku = input_check(flatline, sudoku, MIRACLE)
+    solver = SudokuSolverPycosat(flatline, sudoku)
 
     solver.add_knight_move_constraint()
     solver.add_kings_move_constraint()
