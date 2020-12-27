4c5c4140de55d102b78da1d914439ce841534a72
Thymen Rijpkema <t.d.j.rijpkema@gmail.com>
2020-12-27 01:03:29 +0100

    Cleaned up printing of grid.

--- sudoku/base_solver.py
+++ sudoku/base_solver.py
@@ -27,22 +27,22 @@ class BaseSolver:
         """ Run the actual solver, if successful it will return a new solved Sudoku instance.  """
         raise NotImplementedError
 
-    def render(self, flatline_init=None, flatline_solved=None, n=3):
+    def render(self, flatline_init, flatline_solved=None, n=3):
         fmt = ' | '.join([' %s ' * n] * n)
         sep = ' + '.join([' - ' * n] * n)
         for i in range(n):
             for j in range(n):
                 offset = (i * n + j) * n ** 2
 
-                if flatline_init is not None:
+                if flatline_solved:
                     print(fmt % tuple(flatline_init[offset:offset + n ** 2]), end='\t\t')
-
-                if flatline_solved is not None:
-                    print(fmt % tuple(flatline_solved[offset:offset + n ** 2]), end='')
-                print('')
+                    print(fmt % tuple(flatline_solved[offset:offset + n ** 2]))
+                else:
+                    print(fmt % tuple(flatline_init[offset:offset + n ** 2]))
 
             if i != n - 1:
-                if flatline_init and flatline_solved:
-                    print(f"{sep}\t\t{sep}")
-                else:
-                    print(f"{sep}")
+                print(f"{sep}\t\t{sep}") if flatline_solved else print(f"{sep}")
+   
+    def render_side_by_side(self, fmt, offset, flatline_init, flatline_solved, n=3):
+        print(fmt % tuple(flatline_init[offset:offset + n ** 2]), end='\t\t')
+        print(fmt % tuple(flatline_solved[offset:offset + n ** 2]))
